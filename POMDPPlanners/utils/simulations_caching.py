26fdcff6438c32b6bcf8d2fb8f01143846a248ee
yaacovpariente <yacov.pariente@mail.huji.ac.il>
2025-05-01 14:38:06 +0300

    Add caching utility for simulation results and logging integration</message>
    <message>Introduced a new caching utility in simulations_caching.py to store and retrieve simulation results using diskcache, enhancing performance and efficiency. Implemented functions to generate cache keys and manage cache directories. Integrated logging to provide informative messages about cache hits and misses, improving traceability during simulations. Additionally, added a logger configuration in logger.py for consistent logging across the module.

---
+++ POMDPPlanners/utils/simulations_caching.py
@@ -0,0 +1,57 @@
+import diskcache
+
+from pathlib import Path
+from typing import List
+from POMDPPlanners.core.simulation import History
+from POMDPPlanners.core.environment import Environment
+from POMDPPlanners.core.policy import Policy
+from POMDPPlanners.core.belief import Belief
+from POMDPPlanners.utils.logger import logger
+
+def get_cache_key(
+    environment: Environment,
+    policy: Policy,
+    initial_belief: Belief,
+    general_config: dict = {},
+) -> str:
+    return (environment.config_id, policy.config_id, initial_belief.config_id, str(sorted(general_config.items())))
+
+def get_cache_dir_path(
+    cache_dir_path: Path,
+) -> Path:
+    return cache_dir_path / "simulations_cache"
+
+def cache_simulation_results(
+    environment: Environment,
+    policy: Policy,
+    initial_belief: Belief,
+    results: List[History],
+    cache_dir_path: Path,
+    general_config: dict = {},
+) -> None:
+    cache_dir_path = get_cache_dir_path(cache_dir_path)
+    cache = diskcache.Cache(cache_dir_path)
+
+    key = get_cache_key(environment, policy, initial_belief, general_config)
+    if key in cache:
+        logger.info(f"Simulation results for {key} already cached")
+    else:
+        cache[key] = results
+
+def load_simulation_results(
+    environment: Environment,
+    policy: Policy,
+    initial_belief: Belief,
+    cache_dir_path: Path,
+    general_config: dict = {},
+) -> List[History]:
+    cache_dir_path = get_cache_dir_path(cache_dir_path)
+    cache = diskcache.Cache(cache_dir_path)
+
+    key = get_cache_key(environment, policy, initial_belief, general_config)
+    
+    if key in cache:
+        return cache[key]
+    else:
+        logger.debug(f"Simulation results for {key} not found in cache")
+        return []
