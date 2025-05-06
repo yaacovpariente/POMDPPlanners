a7407683f403a5859c7828eff4e8e20f9afd7ddf
yaacovpariente <yacov.pariente@mail.huji.ac.il>
2025-05-06 21:15:07 +0300

    Update setup.py for package metadata and dependencies</message>
    <message>Enhanced the setup.py file by updating the package version to 0.1.0 and adding author information, a description, and a URL for the project repository. Included classifiers to specify development status and intended audience, and defined Python version requirements. Added an extras_require section for development dependencies, including testing and code quality tools. These changes improve the package's metadata and usability for developers and users.

--- setup.py
+++ setup.py
@@ -2,11 +2,36 @@ from setuptools import setup, find_packages
 
 setup(
     name="POMDPPlanners",
-    version="0.1",
+    version="0.1.0",
+    author="Yaacov Pariente",
+    author_email="yaacovpar@gmail.com",
+    description="A Python package for POMDP planning algorithms and environments",
+    # long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yaacovpariente/POMDPPlanners",
     packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+    python_requires=">=3.8",
     install_requires=[
         "numpy",
         "matplotlib",
         "mlflow>=2.8.0",
     ],
+    extras_require={
+        "dev": [
+            "pytest",
+            "pylint",
+            "black",
+            "pre-commit",
+        ],
+    },
 ) 
